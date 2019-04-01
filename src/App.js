7403ffd5a590bae003a410e7cb6d7bcf3548c291
Per Eriksson <per_hegg@hotmail.com>
2019-04-01 16:18:05 +0200

    make api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class App extends Component {
+
+  componentDidMount(){
+    getData();
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,3 +15,24 @@ class App extends Component {
 }
 
 export default App;
+
+const getData = async () => {
+  const apiUrl = 'https://demo.kimai.org/api/ping'
+  const user_name = 'susan_super'
+  const user_password = 'api_kitten'
+  let headers = {
+    "X-AUTH-USER":user_name,
+    "X-AUTH-TOKEN":user_password   
+  }
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(apiUrl, {
+        headers: headers
+      })
+      .then(response => {
+      debugger;
+        resolve(response);
+      });
+  });
+};
